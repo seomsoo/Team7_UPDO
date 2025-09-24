266ec6e0a011f567e125962c0fdca78607022978
“codnjs3575” <codnjs3575@naver.com>
2025-09-24 18:46:21 +0900

    🔧 [CHORE] jest 의존성 수정

--- jest.config.ts
+++ jest.config.ts
@@ -1,4 +1,3 @@
-import type { Config } from 'jest'
 import nextJest from 'next/jest'
 
 const createJestConfig = nextJest({
@@ -7,12 +6,11 @@ const createJestConfig = nextJest({
 })
 
 // Add any custom config to be passed to Jest
-const config: Config = {
+const config = {
   coverageProvider: 'v8',
   testEnvironment: 'jsdom',
   // 테스트 전에 실행할 설정 파일을 지정
   setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
 }
-
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
 export default createJestConfig(config)
