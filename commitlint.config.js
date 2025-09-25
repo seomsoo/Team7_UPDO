d3fa8396eca797fe1c10285ded1337ce4c9f339c
“codnjs3575” <codnjs3575@naver.com>
2025-09-25 14:08:48 +0900

    🎨 [STYLE] #11 commitlint 설치 및 설정

--- commitlint.config.js
+++ commitlint.config.js
@@ -10,7 +10,7 @@ const Configuration = {
    */
   parserPreset: {
     parserOpts: {
-      headerPattern: /^\[([A-Z]+)\]\s+#\d+\s+(.+)$/,
+      headerPattern: /^\s*(?:[^\[]+)?\s*\[([A-Z]+)\]\s+#\d+\s+(.+)$/,
       headerCorrespondence: ['type', 'subject'],
     },
   },
@@ -23,19 +23,19 @@ const Configuration = {
       2,
       'always',
       [
-        'FEAT',
-        'FIX',
-        'PERF',
-        'DEL',
-        'DOCS',
-        'STYLE',
-        'TEST',
-        'CHORE',
-        'CI',
-        'DESIGN',
-        'REVERT',
-        'RENAME',
-        'REFACTOR',
+        'FEAT', // ✨ 새 기능 추가
+        'FIX', // 🐛 버그 수정
+        'PERF', // ⚡️ 성능 개선
+        'DEL', // 🔥 코드 혹은 파일 제거
+        'DOCS', // 📝 문서 추가/수정
+        'STYLE', // 🎨 구조 개선/코드 포맷 적용
+        'TEST', // ✅ 추가, 업데이트 혹은 테스트 통과
+        'CHORE', // 🔧 구성 파일 추가/수정
+        'CI', // 💚 CI 빌드 수정
+        'DESIGN', // 💄 UI, 스타일 파일 추가/업데이트
+        'REVERT', // ⏪ 변경 사항 되돌리기
+        'RENAME', // 🚚 자원 위치/이름 변경 (파일, 경로, 라우터 ..)
+        'REFACTOR', // ♻️ 코드 리팩토링
       ],
     ],
     // TYPE은 대문자 강제
@@ -66,4 +66,4 @@ const Configuration = {
   },
 };
 
-export default Configuration;
+module.exports = Configuration;
