2e27a9a451868148f2b785e4ac3a41c5f8ec634a
HBA <goddamncool@naver.com>
2025-09-29 02:38:18 +0900

    🔧 [CHORE] #20 환경 규칙 파일 추가/수정

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,10 +6,13 @@ module.exports = {
     'plugin:@typescript-eslint/recommended', // TS 권장 규칙
     'prettier', // Prettier와 충돌하는 규칙 꺼주기
   ],
-  plugins: ['@typescript-eslint', 'prettier'],
+  plugins: ['@typescript-eslint', 'prettier', 'simple-import-sort'],
   rules: {
-    // Prettier와 일치하지 않으면 경고만 띄움
-    'prettier/prettier': 'warn',
+    // OS 라인엔딩 차이 → ESLint linebreak-style 규칙을 비활성화한다.
+    // (OS 별 줄바꿈(LF/CRLF)으로 인한 불필요한 에러 방지)
+    'linebreak-style': 'off',
+    // Prettier와 불일치 시 경고 (LF 기준 강제)
+    'prettier/prettier': ['warn', { endOfLine: 'lf' }],
     // 사용하지 않는 변수 → 경고만
     '@typescript-eslint/no-unused-vars': 'warn',
     // JSX에서 빈 태그는 self-closing 권장
