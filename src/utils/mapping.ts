54d65d8fa7359f19192bf354765eec0871358c09
“codnjs3575” <codnjs3575@naver.com>
2025-10-15 18:55:49 +0900

    ♻️ [REFACTOR] #114 API 호출을 위한 데이터 Mapping 유틸 함수 작성

---
+++ src/utils/mapping.ts
@@ -0,0 +1,43 @@
+import { tags, locations, TAG_OPTIONS } from '@/constants/tags';
+import { tabs, types, TAB_OPTIONS } from '@/constants/tabs';
+
+type Tag = (typeof tags)[number];
+type Location = (typeof locations)[number];
+type Tab = (typeof tabs)[number];
+type Type = (typeof types)[number];
+
+// 1. Tag <-> Location
+export function TagtoLocation(tag: Tag): Location {
+  const tagToLocationMap = Object.fromEntries(
+    TAG_OPTIONS.filter(o => o.value !== 'default').map(o => [o.value, o.location]),
+  ) as Record<Tag, Location>;
+
+  return tagToLocationMap[tag];
+}
+
+export function LocationtoTag(location: Location): Tag {
+  const locationToTagMap = Object.fromEntries(
+    TAG_OPTIONS.filter(o => o.value !== 'default').map(o => [o.location, o.value]),
+  ) as Record<Location, Tag>;
+
+  return locationToTagMap[location];
+}
+
+// 2. Tab <-> Type
+export function TabtoType(tab: Tab): Type {
+  const tabToTypeMap = Object.fromEntries(TAB_OPTIONS.map(o => [o.value, o.type])) as Record<
+    Tab,
+    Type
+  >;
+
+  return tabToTypeMap[tab];
+}
+
+export function TypetoTab(type: Type): Tab {
+  const typeToTabMap = Object.fromEntries(TAB_OPTIONS.map(o => [o.type, o.value])) as Record<
+    Type,
+    Tab
+  >;
+
+  return typeToTabMap[type];
+}
