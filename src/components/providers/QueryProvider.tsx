adef392da2bdb9b9c330ba601079f2e9845b1000
Seo Minsu <minsu9812@naver.com>
2025-10-22 03:40:41 +0900

    ✨ [FEAT] #139 QueryClient의 기본 옵션 설정 추가

--- src/components/providers/QueryProvider.tsx
+++ src/components/providers/QueryProvider.tsx
@@ -5,8 +5,18 @@ import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { useState } from 'react';
 
 export default function QueryProvider({ children }: { children: React.ReactNode }) {
-  const [queryClient] = useState(() => new QueryClient());
-
+  const [queryClient] = useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            staleTime: 1000 * 60 * 3,
+            gcTime: 1000 * 60 * 10,
+            refetchOnWindowFocus: false,
+          },
+        },
+      }),
+  );
   return (
     <QueryClientProvider client={queryClient}>
       {children}
