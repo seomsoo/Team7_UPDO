ac628a87213b61e9ca2796faf9ca19a07b38356d
HBA <goddamncool@naver.com>
2025-10-24 23:18:17 +0900

    Revert "♻️ [REFACTOR] #162 teamId 직접 처리 로직 제거 및 타입 정의 수정"
    This reverts commit 7f0f81b09fde1634b5864bed92efa16c7049d8ff.

--- src/types/gatherings/params.ts
+++ src/types/gatherings/params.ts
@@ -3,11 +3,13 @@ import { Type, Location } from '@/utils/mapping';
 
 // GET /gatherings/{id}
 export interface GetGatheringDetailParams {
+  teamId: string;
   id: number;
 }
 
 // GET /gatherings/{id}/participants
 export interface GetParticipantsParams {
+  teamId: string;
   id: number;
   limit?: number;
   offset?: number;
@@ -17,16 +19,19 @@ export interface GetParticipantsParams {
 
 // PUT /gatherings/{id}/cancel
 export interface CancelGatheringParams {
+  teamId: string;
   id: number;
 }
 
 // POST /gatherings/{id}/join
 export interface JoinGatheringParams {
+  teamId: string;
   id: number;
 }
 
 // DELETE /gatherings/{id}/leave
 export interface LeaveGatheringParams {
+  teamId: string;
   id: number;
 }
 
