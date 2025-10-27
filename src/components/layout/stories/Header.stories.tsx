6c5fc276618f07df399fa146574df587e0de4948
Seo Minsu <minsu9812@naver.com>
2025-10-27 17:04:26 +0900

    🐛[FIX] #150 header 스토리북 수정

--- src/components/layout/stories/Header.stories.tsx
+++ src/components/layout/stories/Header.stories.tsx
@@ -13,41 +13,9 @@ const meta = {
       },
     },
   },
-  argTypes: {
-    favoriteCount: {
-      control: { type: 'number', min: 0, max: 99 },
-      description: '찜한 모임 개수',
-    },
-  },
 } satisfies Meta<typeof Header>;
 
 export default meta;
 type Story = StoryObj<typeof meta>;
 
-// 로그인 상태
-export const LoggedIn: Story = {
-  args: {
-    favoriteCount: 5,
-  },
-};
-
-// 로그아웃 상태
-export const LoggedOut: Story = {
-  args: {
-    favoriteCount: 0,
-  },
-};
-
-// Badge 없음
-export const NoBadge: Story = {
-  args: {
-    favoriteCount: 0,
-  },
-};
-
-// Badge 최대치
-export const MaxBadge: Story = {
-  args: {
-    favoriteCount: 100,
-  },
-};
+export const LoggedIn: Story = {};
