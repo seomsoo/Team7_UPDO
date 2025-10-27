68df4006c73c8f1d9c29a8fbfd29000bcc3ce61d
Seo Minsu <minsu9812@naver.com>
2025-10-27 16:47:45 +0900

    🐛 [FIX] #150 상위 props 제거

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -7,17 +7,10 @@ import HeaderNav from './HeaderNav';
 import { cn } from '@/utils/cn';
 
 interface HeaderProps {
-  favoriteCount?: number;
   className?: string;
 }
 
-interface NavItem {
-  label: string;
-  href: string;
-  hasBadge?: boolean;
-}
-
-export default function Header({ favoriteCount = 0, className }: HeaderProps) {
+export default function Header({ className }: HeaderProps) {
   return (
     <header className={cn('sticky top-0 z-50 w-full bg-[var(--canvas)]', className)}>
       <div className="layout-container m-auto w-full px-4 md:px-6">
@@ -39,7 +32,7 @@ export default function Header({ favoriteCount = 0, className }: HeaderProps) {
             </Link>
 
             {/* Navigation */}
-            <HeaderNav favoriteCount={favoriteCount} />
+            <HeaderNav />
           </div>
 
           {/* Right: profile(Login or MyPage) */}
